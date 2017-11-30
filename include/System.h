3733c110a0288d4c2919f8937ff5abefbb86b06f
Unknown <lukasfrischknecht@me.com>
2017-11-30 14:03:44 +0000

    make map public
    map now public in order to be accesible for ros use later.

--- include/System.h
+++ include/System.h
@@ -122,6 +122,9 @@ public:
     std::vector<MapPoint*> GetTrackedMapPoints();
     std::vector<cv::KeyPoint> GetTrackedKeyPointsUn();
 
+    // Map structure that stores the pointers to all KeyFrames and MapPoints.
+    Map* mpMap;
+
 private:
 
     // Input sensor
@@ -133,8 +136,7 @@ private:
     // KeyFrame database for place recognition (relocalization and loop detection).
     KeyFrameDatabase* mpKeyFrameDatabase;
 
-    // Map structure that stores the pointers to all KeyFrames and MapPoints.
-    Map* mpMap;
+    
 
     // Tracker. It receives a frame and computes the associated camera pose.
     // It also decides when to insert a new keyframe, create some new MapPoints and
